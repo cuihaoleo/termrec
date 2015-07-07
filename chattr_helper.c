d1e0f5c49689de2eef766653f2d996b4da7bc7f8
CUI Hao <cuihao.leo@gmail.com>
2015-07-07 17:58:26 +0800

    Make file immutable after logging

--- chattr_helper.c
+++ chattr_helper.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -7,32 +8,43 @@
 #include <linux/fs.h>
 
 int main (int argc, char* argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Please specify a valid path.");
+    if (argc < 3) {
+showhelp:
+        fprintf(stderr, "Usage: %s [immutable|appendonly] <files>\n", argv[0]);
         exit(-1);
     }
 
+    int addflags, blkflags;
+    if (!strcmp(argv[1], "immutable"))
+        addflags = FS_IMMUTABLE_FL,
+        blkflags = FS_APPEND_FL | FS_SYNC_FL;
+    else if (!strcmp(argv[1], "appendonly"))
+        addflags = FS_APPEND_FL | FS_SYNC_FL,
+        blkflags = FS_IMMUTABLE_FL;
+    else
+        goto showhelp;
+
     int i;
-    int fd;
-    for (i=1; i<argc; i++) {
+    for (i=2; i<argc; i++) {
         char *filename = argv[i];
+        int fd;
 
         if ((fd = open(filename, O_RDONLY)) < 0 &&
             (fd = open(filename, O_RDONLY|O_CREAT, 0600)) < 0) {
             fprintf(stderr, "Cannot open or create file: %s\n", filename);
-            exit(-1);
+            continue;
         }
 
         unsigned long flags;
         ioctl(fd, FS_IOC_GETFLAGS, &flags);
-        flags |= FS_APPEND_FL | FS_SYNC_FL;
-        if (ioctl(fd, FS_IOC_SETFLAGS, &flags)) {
+
+        flags &= ~blkflags;
+        flags |= addflags;
+
+        if (ioctl(fd, FS_IOC_SETFLAGS, &flags))
             fprintf(stderr, "ioctl failed: %s\n", filename);
-            exit(-1);
-        }
 
         close(fd);
-        chmod(filename, S_IRUSR|S_IWUSR);
     }
 
     return 0;
